0b6647dffd24b6b04d50930a3eeb47b4965cb5ea
Deva Midhun <admin@coderrrrr.site>
2024-10-13 17:25:13 +0530

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
-  title: 'Sink',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  title: 'Shink',
+  description: 'Lol my own shorturl :3 :)',
+  image: '',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
