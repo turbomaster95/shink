07a7289ab3d424eab0ddf69d7bc1af391eb68700
Deva Midhun <admin@coderrrrr.site>
2024-10-13 17:45:37 +0530

    Update Footer.vue

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -14,21 +14,13 @@ onMounted(() => {
         href="/"
         class="text-xl font-black leading-none text-gray-900 select-none dark:text-gray-100 logo"
         title="Sink"
-      >Sink</a>
-      <a
-        class="mt-4 text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l sm:border-gray-200 sm:mt-0"
-        href="https://html.zone"
-        target="_blank"
-        title="HTML.ZONE"
-      >
-        &copy; {{ new Date().getFullYear() }} Products of HTML.ZONE
-      </a>
+      >Shink</a>
       <span
         class="inline-flex justify-center mt-4 space-x-5 sm:ml-auto sm:mt-0 sm:justify-start"
       >
         <a
           ref="email"
-          href="mailto:sink.cool$miantiao.me"
+          href="mailto:admin$coderrrrr.site"
           title="Email"
           class="text-gray-400 hover:text-gray-500"
         >
@@ -38,18 +30,7 @@ onMounted(() => {
           />
         </a>
         <a
-          href="https://t.me/htmlzone"
-          target="_blank"
-          title="Telegram"
-          class="text-gray-400 hover:text-gray-500"
-        >
-          <span class="sr-only">Telegram</span>
-          <TelegramIcon
-            class="w-6 h-6"
-          />
-        </a>
-        <a
-          href="https://mt.ci"
+          href="https://coderrrrr.site"
           target="_blank"
           title="Blog"
           class="text-gray-400 hover:text-gray-500"
@@ -61,7 +42,7 @@ onMounted(() => {
         </a>
 
         <a
-          href="https://x.com/0xKaiBi"
+          href="https://x.com/coderrrrrsite"
           target="_blank"
           title="Twitter"
           class="text-gray-400 hover:text-gray-500"
@@ -73,7 +54,7 @@ onMounted(() => {
         </a>
 
         <a
-          href="https://miantiao.me/@chi"
+          href="https://mastodon.social/@coderrrrr"
           target="_blank"
           title="Mastodon"
           class="text-gray-400 hover:text-gray-500"
@@ -85,7 +66,7 @@ onMounted(() => {
         </a>
 
         <a
-          href="https://github.com/ccbikai"
+          href="https://github.com/turbomaster95"
           target="_blank"
           title="GitHub"
           class="text-gray-400 hover:text-gray-500"
