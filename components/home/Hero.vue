d7ee42e927dbed1c7abea6d587894682d62249fe
Deva Midhun <admin@coderrrrr.site>
2024-10-13 17:40:40 +0530

    Update Hero.vue

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -1,8 +1,5 @@
 <script setup>
 import { AreaChart } from 'lucide-vue-next'
-import { GitHubIcon } from 'vue3-simple-icons'
-import heroImg from '@/assets/images/hero.svg?raw'
-
 const { title, description } = useAppConfig()
 </script>
 
@@ -32,27 +29,7 @@ const { title, description } = useAppConfig()
           />
           Dashboard
         </HomeLink>
-        <HomeLink
-          size="lg"
-          type="outline"
-          rel="noopener"
-          href="https://github.com/ccbikai/sink"
-          title="Github"
-          class="flex items-center justify-center gap-1"
-          target="_blank"
-        >
-          <GitHubIcon
-            class="w-5 h-5"
-          />
-          GitHub Repo
-        </HomeLink>
       </div>
-    </div>
-    <div class="hidden py-6 md:block">
-      <div
-        class="w-[512px]"
-        v-html="heroImg"
-      />
     </div>
   </main>
 </template>
