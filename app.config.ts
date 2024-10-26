cda74188481b4e03caa69361d96540a70c04e95c
Deva Midhun <admin@coderrrrr.site>
2024-10-26 13:18:08 +0530

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,10 +1,12 @@
 export default defineAppConfig({
   title: 'Shink',
-  description: 'Lol my own shorturl :3 :)',
+  description: 'A Custom URL Shortener for Me! :3 :3',
   image: '',
   previewTTL: 24 * 3600, // 24h
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
     'dashboard',
+    'admin',
+    'app'
   ],
 })
